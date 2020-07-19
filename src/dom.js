f2b3a72b7e4e43af34d5fe91b6da71d8c6fc15bc
Melodyn <samelodyn@gmail.com>
2020-07-19 17:10:13 +0300

    added ignore for fixtures, fixes

--- src/dom.js
+++ src/dom.js
@@ -1,32 +1,8 @@
 import fs from 'fs';
-import path from 'path';
 import cheerio from 'cheerio';
-import axios from 'axios';
+import { getDomain } from './pathToolkit.js';
 
 const l = console.log;
-
-const getDomain = (hostname) => {
-  const iter = (symbolCount, domain) => {
-    if (symbolCount < 0) return domain;
-    const currentSymbol = hostname[symbolCount];
-    return (currentSymbol === '.' && domain.includes(currentSymbol))
-      ? domain
-      : iter(symbolCount - 1, currentSymbol + domain);
-  };
-  return iter(hostname.length - 1, '');
-};
-const urlPathToName = (urlPath) => {
-  const { dir, name, ext } = path.parse(urlPath);
-  const processedName = path.join(dir, name).match(/\w*/gi)
-    .filter((x) => x)
-    .join('-');
-  return processedName + ext;
-};
-const urlToName = (url, postfix = '') => {
-  const { hostname, pathname } = new URL(url);
-  const name = urlPathToName(`${hostname}${pathname}`);
-  return name + postfix;
-};
 
 const html = fs.readFileSync('./__fixtures__/hexlet-io-courses.html');
 const $ = cheerio.load(html);
--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 import { promises as fs } from 'fs';
 import path from 'path';
 import axios from 'axios';
+import { urlToName } from './pathToolkit.js';
 
-const getDomain = (hostname) => {
-  const iter = (symbolCount, domain) => {
-    const currentSymbol = hostname[symbolCount];
-    return (currentSymbol === '.' && domain.includes(currentSymbol))
-      ? domain
-      : iter(symbolCount - 1, currentSymbol + domain);
-  };
-  return iter(hostname.length - 1, '');
-};
-const urlToName = (url, postfix) => {
-  const { hostname, pathname } = new URL(url);
-  const name = `${hostname}${pathname}`.match(/\w*/gi)
-    .filter((x) => x)
-    .join('-');
-  return name + postfix;
-};
 const createFile = (dirpath, filename, content) => fs
   .writeFile(path.resolve(dirpath, filename), content);
 
 const pageLoader = (url, outputDirPath) => axios.get(url)
   .then(({ data }) => data)
-  .then((data) => createFile(outputDirPath, urlToName(url, '.html'), data));
+  .then((data) => createFile(outputDirPath, urlToName(new URL(url), '.html'), data));
 
 export default pageLoader;
