1b86cf24f36f98e72b1854a32ccc15b977f66422
Melodyn <samelodyn@gmail.com>
2020-08-11 20:49:56 +0300

    refactor fixtures, download as streams

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,26 +5,46 @@ import nock from 'nock';
 import pageLoader from '../index.js';
 import * as pathToolkit from '../src/pathToolkit.js';
 
-const url = new URL('https://hexlet.io/courses');
+const baseUrl = 'https://hexlet.io';
+const urlPath = '/courses';
+const url = new URL(urlPath, baseUrl);
 const domain = pathToolkit.getDomain(url);
 const expectedContent = 'Hello from hexlet.io courses!';
 const expectedFilename = 'hexlet-io-courses.html';
 
 let tmpDirPath = '';
+let fixturePaths = [];
 const fileExists = (dirpath, filename) => fs.readdir(dirpath)
   .then((filenames) => filenames.includes(filename));
 const readFile = (dirpath, filename) => fs.readFile(path.join(dirpath, filename), 'utf-8');
+const getFixturePaths = (currentDirPath) => fs.readdir(currentDirPath, { withFileTypes: true })
+  .then((children) => children
+    .flatMap((child) => (child.isDirectory()
+      ? getFixturePaths(path.join(currentDirPath, child.name))
+      : Promise.resolve({ dirname: currentDirPath, filename: child.name }))))
+  .then((paths) => Promise.all(paths).then((arr) => arr.flat()))
+  .then((paths) => console.log(paths));
+  // .then((paths) => paths.map(({ dirname, filename }) => readFile(dirname, filename)));
+
+nock.disableNetConnect();
 
 beforeAll(async () => {
   tmpDirPath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  fixturePaths = await getFixturePaths('__fixtures__');
 });
 
 test('tmp dir', async () => {
+  // console.log(fixturePaths[0]);
   const fileAlreadyExist = await fileExists(tmpDirPath, expectedFilename);
   expect(fileAlreadyExist).toBe(false);
 
-  nock(new RegExp(domain)).get(/.*/).reply(200, expectedContent);
+  const scope = nock(new RegExp(domain)).get(/.*/)
+    .reply(200, () =>
+      // console.log('path:', this.req.path);
+      expectedContent);
+    // .reply(200, expectedContent);
   await pageLoader(url, tmpDirPath);
+  expect(scope.isDone()).toBe(true);
 
   const fileWasCreated = await fileExists(tmpDirPath, expectedFilename);
   expect(fileWasCreated).toBe(true);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { promises as fs } from 'fs';
+import fs from 'fs';
 import path from 'path';
 import axios from 'axios';
 import { urlToName } from './pathToolkit.js';
 
-const createFile = (dirpath, filename, content) => fs
-  .writeFile(path.resolve(dirpath, filename), content);
+const createFile = (dirpath, filename, content) => content
+  .pipe(fs.createWriteStream(path.resolve(dirpath, filename), { encoding: 'utf-8' }));
 
-const pageLoader = (url, outputDirPath) => axios.get(url.toString())
+const pageLoader = (url, outputDirPath) => axios.get(url.toString(), { responseType: 'stream' })
   .then(({ data }) => data)
   .then((data) => createFile(outputDirPath, urlToName(url, '.html'), data));
 
