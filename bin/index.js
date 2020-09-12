c3923f7cc2c51acd4bce31f8344febbd1448240a
Melodyn <samelodyn@gmail.com>
2020-09-12 13:33:28 +0300

    refactoring

--- bin/index.js
+++ bin/index.js
@@ -4,10 +4,11 @@ import program from 'commander';
 import pageLoader from '../index.js';
 
 program.version('1.0.0')
-  .option('--output [path]', 'output path', process.cwd())
+  .option('-o, --output [path]', 'output path', process.cwd())
+  .option('-b, --progress-bar [name]', 'progress bar name: default | silent', 'default')
   .arguments('<link>')
   .action(async (link) => {
-    await pageLoader(link, program.output)
+    await pageLoader(link, program.output, program.progress)
       .catch((err) => {
         console.error(err.toString());
         process.exit(1);
